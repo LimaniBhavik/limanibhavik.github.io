7d81d0abcb0ee4b569f8331b6025518c87b41152
Bhavik Limani <LimaniBhavik@users.noreply.github.com>
2021-01-22 19:59:40 +0530

    Update main.dart

--- 100-Days-of-Flutter/Appbrewery/i_am_rich/lib/main.dart
+++ 100-Days-of-Flutter/Appbrewery/i_am_rich/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // variable to change the state true/false condition and call
-  bool isSelected = true;
 
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
-      ),
-    ),
+      home: Scaffold(
+        // whole Scaffold Background
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I AM RICH...'),
+          backgroundColor: Colors.blueGrey[900],
+          shadowColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.picxy.com/cache/2019/12/13/2af1c53833b1f1f5fe7ebc1cd1df0143.webp'),
+          ),
+        ),
+      ),)
+    ,
   );
 }
