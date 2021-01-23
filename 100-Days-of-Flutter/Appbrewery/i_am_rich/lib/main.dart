ee3b2c61c136a0e8ace52640179ea172494e7c44
Bhavik Limani <LimaniBhavik@users.noreply.github.com>
2021-01-23 19:00:11 +0530

    Update main.dart

--- 100-Days-of-Flutter/Appbrewery/i_am_rich/lib/main.dart
+++ 100-Days-of-Flutter/Appbrewery/i_am_rich/lib/main.dart
@@ -7,13 +7,20 @@ void main() {
       home: Scaffold(
         // whole Scaffold Background
         backgroundColor: Colors.blueGrey,
+        // AppBar Widget
         appBar: AppBar(
-          title: Text('I AM RICH...'),
+          // AppBar Title Text
+          title: Text('I am Rich...!'),
+          // AppBar Title Background 
           backgroundColor: Colors.blueGrey[900],
+          // AppBar Title Shadow Color
           shadowColor: Colors.blueGrey[900],
         ),
+        // Center Widget
         body: Center(
+          // Image Widget
           child: Image(
+            // Network Image fetch Image from the URL
             image: NetworkImage('https://images.picxy.com/cache/2019/12/13/2af1c53833b1f1f5fe7ebc1cd1df0143.webp'),
           ),
         ),
